d66e8c5d0302dfd836bb13d89fc54936f296c39b
sam102012 <kosrttp103@gmail.com>
2025-06-24 14:40:36 +0900

    Update FamilyGUIHandler.java

--- src/main/java/world/world/FamilyGUIHandler.java
+++ src/main/java/world/world/FamilyGUIHandler.java
@@ -400,11 +400,16 @@ public class FamilyGUIHandler implements GUIHandler, Listener {
             // 팀원 수에 따른 다이아몬드 소모량 하드코딩
             int requiredDiamonds = 0; // 초기값
             if (teamSize == 1) {
-                requiredDiamonds = 64; // 1명일 때 64개
+                requiredDiamonds = 4; // 1명일 때 64개
             } else if (teamSize == 2) {
-                requiredDiamonds = 128; // 2명일 때 128개
-            } else if (teamSize == 3) {
-                requiredDiamonds = 256; // 3명일 때 256개
+                requiredDiamonds = 8; // 2명일 때 128개
+            } else if (teamSize == 3) 
+                requiredDiamonds = 16;// 3명일 때 256개
+            } else if (teamSize == 4) {
+                requiredDiamonds = 32
+            } else if (teamSize == 5) {
+                requiredDiamonds = 64
+                
             }
 
             lore.add(ChatColor.GREEN + "클릭 시 다이아몬드 " + requiredDiamonds + "개 소모.");
